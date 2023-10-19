8e5b97f440c3fc0d418b33d5cd21f5970fc4e41e
Xuan <132154526+Gddwg@users.noreply.github.com>
2023-10-19 09:31:28 +0800

    demo4

---
+++ src/demo4/Test.java
@@ -0,0 +1,10 @@
+package demo4;
+
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        IQueue<String> stringQueue = new IQueue<>(12);
+    }
+}
